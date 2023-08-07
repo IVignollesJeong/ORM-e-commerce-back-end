29c955a1505932ad8681b39c0055dcb0eb1988b2
Ian Vignolles-Jeong <ianvignolles@gmail.com>
2023-08-06 22:50:38 -0400

    add logic to models/index.js

--- models/index.js
+++ models/index.js
@@ -4,13 +4,17 @@ const Category = require('./category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belongsTo Category - product.belongsTo
+Product.belongsTo(Category, {foreignKey: 'category_id'});
 
-// Categories have many Products
+// Categories have many Products - category.hasMany
+Category.hasMany(Product, {foreignKey: 'category_id', onDelete: 'CASCADE'});
 
-// Products belongToMany Tags (through ProductTag)
+// Products belongToMany Tags (through ProductTag) - product.belongsToMany
+Product.belongsToMany(Tag, {through: ProductTag, foreignKey: 'product_id'});
 
-// Tags belongToMany Products (through ProductTag)
+// Tags belongToMany Products (through ProductTag) - tag.belongsToMany
+Tag.belongsToMany(Product, {through: ProductTag, foreignKey: 'tag_id', onDelete: 'CASCADE'});
 
 module.exports = {
   Product,
